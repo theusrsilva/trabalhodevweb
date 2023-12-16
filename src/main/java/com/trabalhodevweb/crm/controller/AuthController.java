cdde0bc404e5cf097a3dabec49bb3a817bb10698
Matheus Rocha <theusrsilva@gmail.com>
2023-12-16 04:15:53 -0300

    bearer token quase funcionando, falta detalhe

--- src/main/java/com/trabalhodevweb/crm/controller/AuthController.java
+++ src/main/java/com/trabalhodevweb/crm/controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.trabalhodevweb.crm.controller;
 
+import com.trabalhodevweb.crm.infra.security.TokenService;
 import com.trabalhodevweb.crm.model.Cargo;
 import com.trabalhodevweb.crm.model.Usuario;
 import com.trabalhodevweb.crm.model.dto.AuthenticationDTO;
+import com.trabalhodevweb.crm.model.dto.LoginResponseDTO;
 import com.trabalhodevweb.crm.model.dto.RegisterDTO;
 import com.trabalhodevweb.crm.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,8 @@ public class AuthController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenService tokenService;
 
     @Autowired
     private UsuarioRepository usuarioRepository;
@@ -29,14 +33,15 @@ public class AuthController {
     public ResponseEntity login(@RequestBody @Validated AuthenticationDTO data){
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.senha());
         var auth = this.authenticationManager.authenticate(usernamePassword);
-        return ResponseEntity.ok().build();
+        var token = tokenService.generateToken((Usuario) auth.getPrincipal());
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
     @PostMapping("/registrar")
     public ResponseEntity register(@RequestBody @Validated RegisterDTO data){
         if(this.usuarioRepository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
         String senhaEncriptada = new BCryptPasswordEncoder().encode(data.senha());
-        Usuario novoUsuario = new Usuario(data.login(), data.nome(), senhaEncriptada, data.email(), Cargo.USUARIO);
+        Usuario novoUsuario = new Usuario(data.login(), data.nome(), senhaEncriptada, data.email(), Cargo.ADMINISTRADOR);
 
         this.usuarioRepository.save(novoUsuario);
         return ResponseEntity.ok().build();
---
+++ src/main/java/com/trabalhodevweb/crm/model/dto/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.trabalhodevweb.crm.model.dto;
+
+public record LoginResponseDTO(String token) {
+}
